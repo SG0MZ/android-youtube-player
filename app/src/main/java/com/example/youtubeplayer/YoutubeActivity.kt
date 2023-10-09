cbe57a97b94d11c53c1bdefc5ec2defdffe49ca2
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-09 11:27:47 -0600

    Youtube Activity - First Commit

---
+++ app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.youtubeplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+    YouTube Android Player API
+
+    This SDK has been fully deprecated. It is no longer supported or maintained by YouTube.
+
+    I can't realize this section of the course.
+ */
+class YoutubeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+    }
+}
